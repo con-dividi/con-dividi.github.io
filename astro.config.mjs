95ebc0cb8aa16f56953deaf22a32c397be6a2089
Eliaxie <elia.maggioni@gmail.com>
2023-06-13 22:59:47 +0200

    tests gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,47 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import WindiCSS from 'vite-plugin-windicss';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { SITE } from './src/config.mjs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
+
+const DEFAULT_OPTIONS = {
+	test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+	exclude: undefined,
+	include: undefined,
+	includePublic: true,
+	logStats: true,
+	ansiColors: true,
+	png: {
+		// https://sharp.pixelplumbing.com/api-output#png
+		quality: 100,
+	},
+	jpeg: {
+		// https://sharp.pixelplumbing.com/api-output#jpeg
+		quality: 100,
+	},
+	jpg: {
+		// https://sharp.pixelplumbing.com/api-output#jpeg
+		quality: 100,
+	},
+	tiff: {
+		// https://sharp.pixelplumbing.com/api-output#tiff
+		quality: 100,
+	},
+	// gif does not support lossless compression
+	// https://sharp.pixelplumbing.com/api-output#gif
+	gif: {},
+	webp: {
+		// https://sharp.pixelplumbing.com/api-output#webp
+		lossless: true,
+	},
+	avif: {
+		// https://sharp.pixelplumbing.com/api-output#avif
+		lossless: true,
+	},
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,7 +63,7 @@ export default defineConfig({
 		}),
 	],
 	vite: {
-		plugins: [WindiCSS()],
+		plugins: [WindiCSS(), ViteImageOptimizer(DEFAULT_OPTIONS)],
 		resolve: {
 			alias: {
 				'~': path.resolve(__dirname, './src'),
