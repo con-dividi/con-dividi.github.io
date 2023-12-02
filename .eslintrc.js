beedbadded9f8d723872ca7ccd4481e8e22728ac
Elia Maggioni <elia.maggioni@gmail.com>
2023-12-02 20:01:40 +0100

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,44 @@
 module.exports = {
-  // ...
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+  },
   overrides: [
     {
-      // Define the configuration for `.astro` file.
-      files: ["*.astro"],
-      // Enable this plugin
-      plugins: ["astro"],
       env: {
-        // Enables global variables available in Astro components.
         node: true,
-        "astro/astro": true,
-        es2020: true,
       },
-      // Allows Astro components to be parsed.
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.astro"],
       parser: "astro-eslint-parser",
-      // Parse the script in `.astro` as TypeScript by adding the following configuration.
-      // It's the setting you need when using TypeScript.
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
-        // The script of Astro components uses ESM.
-        sourceType: "module",
       },
       rules: {
-        // Enable recommended rules
-        "astro/no-conflict-set-directives": "error",
-        "astro/no-unused-define-vars-in-style": "error",
-
-        // override/add rules settings here, such as:
-        // "astro/no-set-html-directive": "error"
+        "@typescript-eslint/no-explicit-any": "off",
       },
     },
-    {
-      // Define the configuration for `<script>` tag.
-      // Script in `<script>` is assigned a virtual file name with the `.js` extension.
-      files: ["**/*.astro/*.js", "*.astro/*.js"],
-      env: {
-        browser: true,
-        es2020: true,
-      },
-      parserOptions: {
-        sourceType: "module",
-      },
-      rules: {
-        // override/add rules settings here, such as:
-        // "no-unused-vars": "error"
-
-        // If you are using "prettier/prettier" rule,
-        // you don't need to format inside <script> as it will be formatted as a `.astro` file.
-        "prettier/prettier": "off",
-      },
-    },
-    {
-      // Configuration for `.mjs` files
-      files: ["*.mjs"],
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-      },
-      // Add any additional rules or settings specific to `.mjs` files here
-    },
-    {
-      // Configuration for JSON files
-      files: ["*.json"],
-      parser: "json-eslint-parser",
-      // Add any additional rules or settings specific to JSON files here
-    },
-    // ...
   ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
